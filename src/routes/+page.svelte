3e49fd68f9ad7f52dfef821b2544fe215525b0a0
crb331 <115194598+crb331@users.noreply.github.com>
2022-10-21 09:22:15 -0500

    create a new template sveltekit-tauri-tailwindcss

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let count = 0;
+    function aumentar() {
+        count++;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<h1>{count}</h1>
+
+<button on:click={aumentar}>Aumentar </button>
+
+<h1 class="text-3xl font-bold">
+    Hello world!
+</h1>
